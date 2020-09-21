665982560072c9d46bdac8581a6220210565003e
Oleg <oleg@sewport.com>
2020-09-21 22:24:43 +0300

    Lesson files

---
+++ lesson_5/functions.py
@@ -0,0 +1,14 @@
+# def test():
+#    return 0
+
+
+# print('module')
+
+# if __name__ == '__main__':
+#    print(__name__)
+#    print(__file__)
+
+file = open('text.txt', 'w', encoding='UTF-8')
+file.write('Hello GB')
+file.write('NEW HELLo')
+file.close()
